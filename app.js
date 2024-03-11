01e3dd2ece924a2cd5a72c1f93522fe159333bcb
midward <midwardisaac@gmail.com>
2024-03-10 19:53:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+gsap.registerPlugin(Flip);
+
+const links = document.querySelectorAll(".nav_item a");
+const activeNav = document.querySelector(".active_nav");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        gsap.to(links, { color: "black" });
+        if (document.activeElement === link) {
+            gsap.to(link, { color: "red" });
+            console.log(link);
+        }
+        moveActiveNav(link);
+    });
+});
+
+function moveActiveNav(link) {
+    const state = Flip.getState(activeNav);
+    link.appendChild(activeNav);
+    Flip.from(state, {
+        duration: 1.25,
+        absolute: true,
+        ease: "elastic.out(1,.5)"
+    });
+}
+
+// Cards handling
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        // Remove active class from all cards
+        cards.forEach((otherCard) => {
+            otherCard.classList.remove("active");
+        });
+        
+        // Add active class to the clicked card
+        card.classList.add("active");
+        
+        // Add is-inactive class to all other cards
+        cards.forEach((otherCard) => {
+            if (otherCard !== card) {
+                otherCard.classList.add("is-inactive");
+            }
+        });
+    });
+});
